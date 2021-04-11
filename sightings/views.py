2578a178c841cf6f83d98e98216ed494f86f0155
Kinjal1606 <kinjal.saxena@columbia.edu>
2021-04-11 15:58:25 -0400

    map views

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+from sightings.models import Squirrel
+
 
 # Create your views here.
+def map(request):
+    sightings= Sighting.objects.all()[:100]
+    context ={
+            'sightings':sightings,
+            }
+    return render(request, 'tracker/map.html', context)
